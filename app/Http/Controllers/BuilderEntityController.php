fbe42849064d8e18035fa0b7a03275b32257a840
Edgar <khachaturovedgar01@gmail.com>
2023-12-06 17:32:56 +0400

    added the connection of BuilderEntityController

---
+++ app/Http/Controllers/BuilderEntityController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PATIENTS;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class BuilderEntityController extends Controller
+{
+    public function buildEntity($leadID){
+        $lead = $this->getLead($leadID);
+        return [
+            'contact'   => $lead ? $this->getContactArray($lead['contactId'], $lead['declareVisit']) : '', // Нужна проверка на первое посещение
+            'lead'      => $lead ?: '',
+        ];
+    }
+
+    private function getLead($leadID) : array{
+        $lead = DB::query()->where('ID', $leadID)->first();
+        if ($lead){
+            return $lead->toArray();
+        }
+        return [];
+    }
+
+    private function getContactArray($contactId, $declareVisit = false) : array{
+        return PATIENTS::all()
+            ->where('id', '=', $contactId)
+            ->first($this->getColumns($declareVisit))->toArray();
+    }
+
+    private function getColumns($declareVisit) : array{
+        $columns = [
+            'NOM', 'PRENOM', 'PATRONYME', // FIO
+            'MOBIL_NYY', // Mobile
+            'EMAIL', // Email
+            'POL', // POL
+            'GOROD', // CITY
+            'NE_LE', //  DATE OF BIRTH
+        ];
+        if ($declareVisit){
+            $columns = array_merge($columns, [
+                'RAYON_VYBORKA', // District
+                'ULICA', // Street
+                'DOM', // DOM
+                'KVARTIRA', // FLAT
+            ]);
+        }
+        return $columns;
+    }
+}
