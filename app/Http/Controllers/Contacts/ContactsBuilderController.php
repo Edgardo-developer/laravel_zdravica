8e382bb54cbd51a6d59ed924e66c1264717f0de9
Edgar <khachaturovedgar01@gmail.com>
2024-02-04 22:01:07 +0400

    changed the getRow of the Contacts

--- app/Http/Controllers/Contacts/ContactsBuilderController.php
+++ app/Http/Controllers/Contacts/ContactsBuilderController.php
@@ -11,7 +11,7 @@ class ContactsBuilderController extends Controller
     public function getRow(int $id, bool $declareCall = false): array
     {
         Log::info($id);
-        $pat = PATIENTS::find($id);
+        $pat = PATIENTS::where('PATIENTS_ID','=',$id);
         Log::info("I found ".$pat->count());
         if($pat->count() > 0){
             return $pat->first()->only(self::getColumns($declareCall ? 1 : 0));
