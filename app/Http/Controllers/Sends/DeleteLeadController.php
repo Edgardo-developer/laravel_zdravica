34aaf59ac40619175ad05d8ddb63f76c2d005bdf
Edgar <khachaturovedgar01@gmail.com>
2024-02-05 19:14:16 +0400

    upgraded the deleteController

--- app/Http/Controllers/Sends/DeleteLeadController.php
+++ app/Http/Controllers/Sends/DeleteLeadController.php
@@ -2,17 +2,18 @@
 
 namespace App\Http\Controllers\Sends;
 
-use App\Http\Controllers\Bill\BillBuilderController;
 use App\Http\Controllers\Bill\BillController;
-use App\Http\Controllers\Bill\BillRequestController;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\Leads\LeadController;
-use App\Http\Controllers\Leads\LeadRequestController;
 use App\Models\AmocrmIDs;
 use GuzzleHttp\Client;
 
 class DeleteLeadController extends Controller
 {
+    private BillController $BillController;
+    private LeadController $LeadController;
+    private array $dbIDs;
+
     public function __construct($ids){
         $client = new Client(['verify'=>false]);
         $this->BillController = new BillController($client);
@@ -20,28 +21,27 @@ class DeleteLeadController extends Controller
         $this->dbIDs = $ids;
     }
 
-    public function deleteLeads(bool $withReason){
+    public function deleteLeads(bool $withReason) : void{
         $leadArray = [];
         $billArray = [];
         foreach ($this->dbIDs as $dbID) {
             $leadID = (int)$dbID;
-            $billID = amocrmIDs::where('amoLeadID', $dbID)->first()->amoBillID;
+            $billID = AmocrmIDs::with('WITH(NOLOCK)')->where('amoLeadID','=', $leadID)->first();
 
             if ($leadID > 0) {
                 $leadArray[] = $withReason ?
                     $this->LeadController->closeLead($leadID) :
                     $this->LeadController->finishLead($leadID);
             }
-            if ($billID > 0) {
+            if ($billID) {
                 $billArray[] = $this->BillController->prepare(
-                    $billID, $withReason ? 0 : 1);
+                    $billID->amoBillID, $withReason ? 0 : 1);
             }
         }
         $this->removeThem($leadArray,$billArray);
-        return true;
     }
 
-    private function removeThem($leadArray, $billArray){
+    private function removeThem($leadArray, $billArray) : void{
         if (count($leadArray[0]) > 0) {
             if (count($billArray) > 0) {
                 $this->BillController->updateBill($billArray, $billArray['billStatus']);
