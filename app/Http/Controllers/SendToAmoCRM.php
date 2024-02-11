b0c63396395bc31a153906a7c04b3dac68a7b98c
Edgar <khachaturovedgar01@gmail.com>
2024-02-11 22:18:53 +0400

    fixed log output

--- app/Http/Controllers/SendToAmoCRM.php
+++ app/Http/Controllers/SendToAmoCRM.php
@@ -28,7 +28,7 @@ class SendToAmoCRM extends Controller
      */
     public function sendDealToAmoCRM(): array
     {
-        Log::info('Creating jobs for DBLead '.$this->DBlead);
+        Log::info('Creating jobs for DBLead '.$this->DBlead['leadDBId']);
         $buildLead = $this->DBlead;
         $buildLead['FIO'] = $this->getPlanningFIO($this->DBlead);
         ksort($buildLead, SORT_NATURAL);
