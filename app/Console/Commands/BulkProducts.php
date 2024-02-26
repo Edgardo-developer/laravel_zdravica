410aff1225cf1169a87926a12b41b5bdff8ec90b
Edgar <khachaturovedgar01@gmail.com>
2024-02-26 14:43:08 +0400

    added test

--- app/Console/Commands/BulkProducts.php
+++ app/Console/Commands/BulkProducts.php
@@ -34,6 +34,7 @@ class BulkProducts extends Command
         $client = new Client(['verify' => false]);
 
         $ProductController = new ProductController($client);
+        dd($offersChunks);
         foreach ($offersChunks as $offersChunk) {
             $preparedProducts = $ProductController->prepare($offersChunk, 1);
             $proids = $ProductController->create($preparedProducts);
